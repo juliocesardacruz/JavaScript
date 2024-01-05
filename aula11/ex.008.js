5d5ba6284b3f93cede554165b7478e838500095b
Julio Cesar <jucecr@hotmail.com>
2024-01-05 20:38:35 -0300

    a

---
+++ aula11/ex.008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60) {// Condição Simples
+    console.log(`Você ultrapassou a velociade permitida. MULTADO!!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brasil') {
+    console.log(`Você é Estrangeiro!!`)
+} else {console.log (`Você é Brasileiro!!`)
+}
