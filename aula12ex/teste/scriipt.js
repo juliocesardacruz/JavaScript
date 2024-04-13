c886482c5cb3ebb655d1394579982a541dbc1fa3
Julio Cesar <jucecr@hotmail.com>
2024-04-13 02:58:44 -0300

    a

---
+++ aula12ex/teste/scriipt.js
@@ -0,0 +1,11 @@
+function executar() {
+    var numero = document.getElementById('numero')
+    var calcular = document.getElementById ('executar')
+    var result = document.getElementById ('res')
+    if (numero % 2 = 0) {
+        window.alert ('ok')
+    } else {
+        window.alert ('no')
+    }
+
+}
