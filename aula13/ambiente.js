e30ded6fa5142b0ae0a32344d9bedccddfc212e8
Julio Cesar <jucecr@hotmail.com>
2024-04-18 00:33:27 -0300

    q

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
+
+
+/*
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+*/
---
+++ aula13/node_9607236688ffa.tmp.js
@@ -0,0 +1,17 @@
+var c = 1
+while (c < 6) {
+    console.log('Tudo Bem')
+}
+
+
+
+
+
+/*
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+*/
