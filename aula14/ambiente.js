071168463c83ffb4b20bad87257fbe0514f3883a
Julio Cesar <jucecr@hotmail.com>
2024-04-22 22:23:14 -0300

    4

---
+++ aula14/ambiente.js
@@ -0,0 +1,18 @@
+/*var c = 1
+while (c <= 5) {
+    console.log (c)
+    c++
+}
+
+var c = 1
+do {
+    console.log (c)
+    c++
+} while (c <= 5)
+*/
+//----------------------------
+
+
+for (c = 1;c <=  5; c++){
+    console.log(c)
+}
