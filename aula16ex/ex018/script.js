52c1e457b3349033ad22342aa7280ed6c293a438
Julio Cesar <jucecr@hotmail.com>
2024-05-09 22:16:51 -0300

    a

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,7 +1,7 @@
 let num = document.querySelector('input#fnum') 
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
-let valores = []
+let valores = [] //Esse vetor vamos utilizar para analisar os dados!!
 
 function isNumero(n) {
     if(Number(n) >= 1  &&  Number(n) <= 100) {
@@ -21,8 +21,40 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        //window.alert('Tudo ok')
+      valores.push(Number(num.value))
+      let item = document.createElement('option')  
+      item.text = `Valor ${num.value} adicionado` 
+      lista.appendChild(item)   
+      res.innerHTML = ''  
     } else {
         window.alert('Valor inválido ou já encontrado na lista!!')
+    }
+    num.value = ''
+    num.focus() //Volta o cursor para a caixa para poder digitar novo número.
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} número(s) cadastrado(s)!!</p>`
+        res.innerHTML += `<p> O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p> O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p> Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p> A média dos valores digitados é ${media}.</p>`
     }
 }
